698f16a586cadeda79b7989de211f88a89cbdfe0
Jan Krecke <jan.krecke@pm.me>
2023-02-07 22:44:47 +0900

    cli parser

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,18 @@
-fn main() {
-    println!("Hello, world!");
+use clap::Parser;
+use std::path::PathBuf;
+
+#[derive(Parser, Default, Debug)]
+#[clap(author = "Jan Krecke", version)]
+/// Find circular imports in a Python project
+struct Arguments {
+    #[clap(forbid_empty_values = true)]
+    /// Path to Python project
+    input_path: PathBuf,
+}
+
+fn main() -> Result<(), Box<dyn std::error::Error>> {
+    let args = Arguments::parse();
+    let input_path = args.input_path;
+
+    return Ok(());
 }

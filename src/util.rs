09f6f0c4c8a0128cfa409fb61a0100bfad6bdd8d
Jan Krecke <jan.krecke@pm.me>
2023-02-23 23:03:19 +0900

    replaced match with simple let statement

--- src/util.rs
+++ src/util.rs
@@ -3,10 +3,7 @@ pub struct UnorderedPair<T>(pub T, pub T);
 
 impl<T: std::cmp::PartialEq> PartialEq for UnorderedPair<T> {
     fn eq(&self, other: &Self) -> bool {
-        match (self, other) {
-            (UnorderedPair(a1, b1), UnorderedPair(a2, b2)) => {
-                (a1 == a2 && b1 == b2) || (a1 == b2 && b1 == a2)
-            }
-        }
+        let (UnorderedPair(a1, b1), UnorderedPair(a2, b2)) = (self, other);
+        (a1 == a2 && b1 == b2) || (a1 == b2 && b1 == a2)
     }
 }
